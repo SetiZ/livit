13f52ae0b956bc0043cd3a4d20c335d370ea6c56
Setiz <setiz@sd-97387.dedibox.fr>
2016-09-12 22:49:34 +0200

    livit assignment w

--- assets/src.js
+++ assets/src.js
@@ -31,47 +31,47 @@ var appVue = new Vue({
       {
         id: 1,
         src: 'http://images.grasshopper.com/2013/10/happy-people-coworking.jpg',
-        title: '1',
+        title: 'Coworking',
         date: '2016',
         site: 'Malaysia',
         like: 4,
-        desc: 'tralala',
+        desc: 'coworking is great!',
       },
       {
         id: 2,
         src: 'http://www.projectmanagers.net/i/wp-content/uploads/2013/02/bigstock-Group-of-happy-young-business-50927732.jpg',
-        title: '2',
+        title: 'Brainstorming',
         date: '2013',
         site: 'Spain',
         like: 1,
-        desc: 'trolo',
+        desc: 'Brainstorming for a new product!',
       },
       {
         id: 3,
         src: 'http://i.huffpost.com/gen/1832336/images/n-HAPPY-EMPLOYEE-large570.jpg',
-        title: '3',
+        title: 'We did it!',
         date: '2016',
         site: 'USA',
         like: 0,
-        desc: 'pouet',
+        desc: 'We won the contract!',
       },
       {
         id: 4,
         src: 'http://il6.picdn.net/shutterstock/videos/5658755/thumb/9.jpg',
-        title: '3',
+        title: 'Team building',
         date: '2015',
         site: 'Sweden',
         like: 3,
-        desc: 'fart',
+        desc: 'Working all together',
       },
       {
         id: 4,
         src: 'http://1.bp.blogspot.com/_AsgGlcNuJtU/S_ClPsz4CII/AAAAAAAAAgI/yboWMv0YSi8/s1600/happy_office_hour.jpg',
-        title: '3',
+        title: 'Celebration',
         date: '2015',
         site: 'Sweden',
         like: 3,
-        desc: 'fart',
+        desc: 'Celebration at the office!',
       },
     ]
   },
@@ -87,24 +87,59 @@ var appVue = new Vue({
       });
       return result;
     },
-    years: function() {
+    currentDisplay: function() {
       var result = new Array();
-      this.images.forEach(function(image){
-        if (result.indexOf(image.date) < 0) {
-            result.push(image.date);
-        }
-      });
-      result.sort().reverse();
-      return result;
-    },
-    cities: function () {
-      var result = new Array();
-      this.images.forEach(function(image) {
-        if (result.indexOf(image.site) < 0) {
-            result.push(image.site);
-        }
-      });
-      result.sort()
+      switch(this.sort) {
+        case "id":
+        result.push({title:"", images: new Array()});
+        this.images.forEach(function(image) {
+          result[0].images.push(image);
+        });
+        result[0].images.sort(function(a, b){
+          return a.id - b.id;
+        });
+        break;
+        case "date":
+        var tmpYears = new Array();
+        this.images.forEach(function(image){
+          if (tmpYears.indexOf(image.date) < 0) {
+            result.push({title: image.date, images: new Array()});
+            tmpYears.push(image.date);
+          }
+          result.forEach(function(year) {
+            if(year.title == image.date) {
+              year.images.push(image);
+            }
+          });
+        });
+        result.sort(function(a, b) {
+          return parseInt(b.title) - parseInt(a.title);
+        });
+        break;
+        case "site":
+        var tmpCities = new Array();
+        this.images.forEach(function(image){
+          if (tmpCities.indexOf(image.site) < 0) {
+            result.push({title: image.site, images: new Array()});
+            tmpCities.push(image.site);
+          }
+          result.forEach(function(city) {
+            if(city.title == image.site) {
+              city.images.push(image);
+            }
+          });
+        });
+        break;
+        case "like":
+        result.push({title:"", images: new Array()});
+        this.images.forEach(function(image) {
+          result[0].images.push(image);
+        });
+        result[0].images.sort(function(a, b){
+          return b.like - a.like;
+        });
+        break;
+      }
       return result;
     }
   },
